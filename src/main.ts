62fd3318a951ab7655bb4342f1cf753e364cb00e
EnderDev <kieran@dothq.org>
2023-08-09 13:18:02 +0100

    ✨ Add end time to json

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,23 @@ const main = () => {
     const merged = json.flat(1)
         .filter(f => f.ms_played >= 30000 || f.reason_done == "trackdone")
         .filter(f => new Date(f.ts) <= cutoff)
-        .sort((a, b) => new Date(a.ts) - new Date(b.ts))
-        .filter(f => f.master_metadata_track_name);
+        .sort((a, b) => new Date(a.ts).getTime() - new Date(b.ts).getTime())
+        .filter(f => f.master_metadata_track_name)
+        .map(f => {
+            const endTime = new Date(f.ts);
+            endTime.setMilliseconds(endTime.getMilliseconds() + f.ms_played);
+
+            return {
+                ...f,
+                endTime: endTime.toISOString()
+            }
+        })
  
     ensureDirSync(resolve(process.cwd(), "dist"));
-    writeFileSync(resolve(process.cwd(), "dist", "streaming_history.json"), JSON.stringify(merged, "", 4));
+    writeFileSync(resolve(process.cwd(), "dist", "streaming_history.json"), JSON.stringify(merged, null, 4));
 
     console.log(merged[0], merged[1], merged[merged.length - 2], merged[merged.length - 1]);
-    console.log(merged.length, merged.length / 3000);
+    console.log(merged.length, merged.length / 2800);
 }
 
 main();
